9bce607fffe55528fb42034ea96de4f10c17f5eb
SalmanKinectro <salman.kinectro@gmail.com>
2022-08-19 18:04:57 +0500

    done

--- src/components/seeWeather/weatherCard.js
+++ src/components/seeWeather/weatherCard.js
@@ -1,29 +1,126 @@
-import { Paper, Stack, Typography } from "@mui/material";
+import { Divider, Paper, Stack, Typography } from "@mui/material";
 import { Box } from "@mui/material";
 import React from "react";
 
 const weatherCard = (weather) => {
+  console.log(weather);
   return (
     <Stack direction="row" justifyContent="center" sx={{ padding: "20px" }}>
-      <Paper
-        elevation={20}
+      <Box
         sx={{
+          padding: "10px",
+          background: "rgba(9, 31, 64,0.8)",
           width: {
             xs: 315,
-            sm: 420,
-            md: 500,
-            lg: "500px",
-            xl: "500px",
           },
-          marginTop: "10px",
-          background: "blue",
-
-          padding: "30px",
-          borderRadius: "25px",
-          borderColor: "error.main",
         }}
+        borderRadius="20px"
       >
-        <Stack direction="row">
+        <Stack direction="row" spacing={3}>
+          <Box
+            component="img"
+            src={weather.img}
+            sx={{
+              height: 150,
+              ml: {
+                xs: -2,
+                xl: 6,
+              },
+            }}
+          />
+          <Typography
+            sx={{
+              mt: {
+                xs: -2,
+              },
+              fontSize: {
+                xs: "60px",
+                xl: "100px",
+              },
+              fontWeight: "500",
+              color: "white",
+            }}
+          >
+            {weather.temp + "\u00b0C"}
+          </Typography>
+        </Stack>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-around",
+            flexWrap: "wrap",
+            p: 1,
+            m: 1,
+            bgcolor: "background.paper",
+            borderRadius: 1,
+          }}
+        >
+          <Typography
+            sx={{
+              fontSize: "20px",
+              fontWeight: "300",
+            }}
+          >
+            {weather.type}
+          </Typography>
+
+          <Typography
+            sx={{
+              fontSize: "20px",
+              fontWeight: "300",
+            }}
+          >
+            {"Wind Speed: " + weather.windSpeed + " m/s"}
+          </Typography>
+          <Typography
+            sx={{
+              fontSize: "20px",
+              fontWeight: "300",
+            }}
+          >
+            {"Humidity: " + weather.humidity + "%"}
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-around",
+            flexWrap: "wrap",
+            p: 1,
+            m: 1,
+            bgcolor: "background.paper",
+            borderRadius: 1,
+          }}
+        >
+          <Typography
+            sx={{
+              fontSize: "20px",
+              fontWeight: "300",
+            }}
+          >
+            Temp_max: {weather.temp_max + "\u00b0C"}
+          </Typography>
+          <Typography
+            sx={{
+              fontSize: "20px",
+              fontWeight: "300",
+            }}
+          >
+            Temp_min: {weather.temp_min + "\u00b0C"}
+          </Typography>
+          <Typography
+            sx={{
+              fontSize: "20px",
+              fontWeight: "300",
+            }}
+          >
+            {"Feels Like: " + weather.feels_like + "\u00b0C"}
+          </Typography>
+        </Box>
+
+        {/* <Stack direction="row">
           <Box
             flex={10}
             component="img"
@@ -79,8 +176,8 @@ const weatherCard = (weather) => {
               {"Humidity: " + weather.humidity + "%"}
             </Typography>
           </Stack>
-        </Stack>
-      </Paper>
+        </Stack> */}
+      </Box>
     </Stack>
   );
 };
